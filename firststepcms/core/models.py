02b91bc46b78e6f10e0f6635efd0b06b4d1bda60
AhmedFahmy-cyber <dedsec5060@gmail.com>
2020-11-11 03:59:58 +0200

    first commit-crud01

---
+++ firststepcms/core/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User 
+
+
+
+
+# Create your models here.
+class Core(models.Model):
+
+    title = models.CharField(max_length=200)
+    excpert = models.TextField(null=True) 
+    author = models.ForeignKey(User , on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=200 , unique=True)
+    image = models.ImageField(upload_to ='uploads/% Y/% m/% d/')
+    updated = models.DateTimeField(auto_now=True)
+    puplished = models.DateTimeField(default=timezone.now)
+
+
+
+    def get_absolute_url(self):
+        return reverse("core:singleview", args=[self.slug] )
+    
+    class Meta:
+
+        ordering = ['-puplished']
+        verbose_name_plural = 'Core'
+
+
+    def __str__(self):
+
+        return self.title     
+
+    
---
+++ firststepcms/core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import ListView , DetailView , UpdateView
+from .models import *
+from django.urls import reverse_lazy
+
+
+
+# Create your views here.
+
+class IndexView(ListView) :
+    
+
+    model = Core
+    template_name = 'core/core_list.html'
+
+
+class SinglelView(DetailView):
+    model = Core
+    template_name = "core/single.html"
+    context_object_name = 'post'
+    
+
+
